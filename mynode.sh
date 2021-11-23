09a07b42b2c6ee612b9c24c140a3fe6dc3f9f8bf
iwbinb <34509975+iwbinb@users.noreply.github.com>
2021-11-23 20:07:52 +0800

    Create mynode.sh

---
+++ mynode.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo -E bash -c "$(curl -s https://raw.githubusercontent.com/mysteriumnetwork/node/master/install.sh)"
+myst cli --agreed-terms-and-conditions 
+mmn vCHip7RUBL8q63EzWz0YbJcyUlHoiMlio70UGxgf
